5f16896c8f3b810405d23db884239acdc2fef8b8
dudu128 <duncan880128@gmail.com>
2023-12-19 18:14:43 +0800

    plus log

--- inference_herbs_api/code/inference_herbs_api.py
+++ inference_herbs_api/code/inference_herbs_api.py
@@ -27,7 +27,11 @@ import io
 import time
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s,%(msecs)03d [%(levelname)s] %(name)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+    )
 logger = logging.getLogger(__name__)
 
 lock = Lock()
